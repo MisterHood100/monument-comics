de12a76b5d0a752d2f272943767613e4ac4c4ae8
MisterHood100 <brandenhood92@gmail.com>
2023-09-25 13:30:21 -0700

    Added Mobile Menu to Homepage

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,16 +1,26 @@
 import "./navbar.css"
 import { Link, NavLink } from "react-router-dom";
+import { useState } from "react";
 
 function NavBar() {
+
+    const [nav, showNav] = useState(true);
+
+    function clicked() {
+        showNav(false);
+    }
+
     return (
         <div className="navbar">
-            <ul className="navBarList">
+            <i onClick={clicked} className="fa-solid fa-bars mobileMenu hide show"></i>
+            {nav ? "" : <ul className="navBarList">
                 <NavLink to="/" end><li className="navBarListItem">HOME</li></NavLink>
                 <NavLink to="/about"><li className="navBarListItem">ABOUT</li></NavLink>
                 <NavLink to="/characters"><li className="navBarListItem">CHARACTERS</li></NavLink>
                 <NavLink to="/blog"><li className="navBarListItem">BLOG</li></NavLink>
                 <Link to="https://monumentcomicsworld.com/collections/all" target="_blank" rel="noopener noreferrer"><li className="navBarListItem">SHOP</li></Link>
-            </ul>
+            </ul>}
+            
         </div>
     )
 }
