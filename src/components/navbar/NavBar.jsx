9884786e081fea0d1b3ad214c118b70bca99cc3b
MisterHood100 <brandenhood92@gmail.com>
2023-09-25 21:21:33 -0700

    Minor Fixes and Updates to Announcement, NavBar.
    Also Updated Scaling and Active State to Links.

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,4 +1,4 @@
-import "./navbar.css"
+import "./navbar.css";
 import { Link, NavLink } from "react-router-dom";
 import { useState } from "react";
 
@@ -24,19 +24,26 @@ function NavBar() {
                 <NavLink to="/blog"><li className="navBarListItem">BLOG</li></NavLink>
                 <Link to="https://monumentcomicsworld.com/collections/all" target="_blank" rel="noopener noreferrer"><li className="navBarListItem">SHOP</li></Link>
             </ul>
-            
-            {nav ? "" 
-            
-            : 
-            
-            <ul className="navBarList">
-            
-                <NavLink to="/" end><li className="navBarListItemMobile">HOME</li></NavLink>
-                <NavLink to="/about"><li className="navBarListItemMobile">ABOUT</li></NavLink>
-                <NavLink to="/characters"><li className="navBarListItemMobile">CHARACTERS</li></NavLink>
-                <NavLink to="/blog"><li className="navBarListItemMobile">BLOG</li></NavLink>
-                <Link to="https://monumentcomicsworld.com/collections/all" target="_blank" rel="noopener noreferrer"><li className="navBarListItemMobile">SHOP</li></Link>
-            </ul>}     
+
+            {nav ? ""
+
+                :
+
+                <ul className="navBarListMobile">
+
+                    <div className="navBarIconsMobile">
+                        <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer"><i className="navBarSocialIconMobile fa-brands fa-square-instagram"></i></a>
+                        <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer"><i className="navBarSocialIconMobile fa-brands fa-square-twitter"></i></a>
+                        <a href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer"><i className="navBarSocialIconMobile fa-brands fa-youtube"></i></a>
+                        <a href="mailto:bhood@monumentcomicsworld.com"><i className="navBarEmailIconMobile fa-solid fa-envelope"></i></a>
+                    </div>
+
+                    <NavLink to="/" end><li className="navBarListItemMobile">HOME</li></NavLink>
+                    <NavLink to="/about"><li className="navBarListItemMobile">ABOUT</li></NavLink>
+                    <NavLink to="/characters"><li className="navBarListItemMobile">CHARACTERS</li></NavLink>
+                    <NavLink to="/blog"><li className="navBarListItemMobile">BLOG</li></NavLink>
+                    <Link to="https://monumentcomicsworld.com/collections/all" target="_blank" rel="noopener noreferrer"><li className="navBarListItemMobile">SHOP</li></Link>
+                </ul>}
         </div>
     )
 }
