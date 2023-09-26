3a82aac61625618b1d8af5ea62770754a4a12202
MisterHood100 <brandenhood92@gmail.com>
2023-09-25 20:23:15 -0700

    Changed NavBar & Announcement

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -7,20 +7,36 @@ function NavBar() {
     const [nav, showNav] = useState(true);
 
     function clicked() {
-        showNav(false);
+        if (nav) {
+            showNav(false)
+        } else {
+            showNav(true)
+        }
     }
 
     return (
         <div className="navbar">
             <i onClick={clicked} className="fa-solid fa-bars mobileMenu hide show"></i>
-            {nav ? "" : <ul className="navBarList">
+            <ul className="navBarList">
                 <NavLink to="/" end><li className="navBarListItem">HOME</li></NavLink>
                 <NavLink to="/about"><li className="navBarListItem">ABOUT</li></NavLink>
                 <NavLink to="/characters"><li className="navBarListItem">CHARACTERS</li></NavLink>
                 <NavLink to="/blog"><li className="navBarListItem">BLOG</li></NavLink>
                 <Link to="https://monumentcomicsworld.com/collections/all" target="_blank" rel="noopener noreferrer"><li className="navBarListItem">SHOP</li></Link>
-            </ul>}
+            </ul>
             
+            {nav ? "" 
+            
+            : 
+            
+            <ul className="navBarList">
+            
+                <NavLink to="/" end><li className="navBarListItemMobile">HOME</li></NavLink>
+                <NavLink to="/about"><li className="navBarListItemMobile">ABOUT</li></NavLink>
+                <NavLink to="/characters"><li className="navBarListItemMobile">CHARACTERS</li></NavLink>
+                <NavLink to="/blog"><li className="navBarListItemMobile">BLOG</li></NavLink>
+                <Link to="https://monumentcomicsworld.com/collections/all" target="_blank" rel="noopener noreferrer"><li className="navBarListItemMobile">SHOP</li></Link>
+            </ul>}     
         </div>
     )
 }
