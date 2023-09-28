ed561bdd90f25c9e79100aacd852a9b68e5aa011
MisterHood100 <brandenhood92@gmail.com>
2023-09-27 20:21:04 -0700

    Made minor updates and improvements to components.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,12 +3,17 @@ import "./footer.css"
 function Footer() {
     return (
         <div className="footer">
-            <h3 className="footerMessage">FOLLOW MONUMENT COMICS ON SOCIAL MEDIA</h3>
-            <div className="footerSocialIcons">
-                <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer"><i className="footerSocialIcon fa-brands fa-square-instagram"></i></a>
-                <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer"><i className="footerSocialIcon fa-brands fa-square-twitter"></i></a>
-                <a href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer"><i className="footerSocialIcon fa-brands fa-youtube"></i></a>
+            <img className="footerImage" src="../../public/favicon.png" alt="Monument Comics Logo" />
+            <div className="footerSocialMedia">
+                <h4 className="footerMessage">Follow Monument Comics On Social Media</h4>
+                <div className="footerSocialIcons">
+                    <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer"><i className="footerSocialIcon fa-brands fa-square-instagram"></i></a>
+                    <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer"><i className="footerSocialIcon fa-brands fa-square-twitter"></i></a>
+                    <a href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer"><i className="footerSocialIcon fa-brands fa-youtube"></i></a>
+                </div>
             </div>
+            <hr />
+            <h4 className="footerCopyright">&copy; Monument Comics, All Rights Reserved | Designed By Branden Hood</h4>
         </div>
 
     )
